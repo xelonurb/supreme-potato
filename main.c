0286ed7f372cc4f4a72b4c5a7124dd6626ca8309
xelonurb <163100560+xelonurb@users.noreply.github.com>
2025-06-19 00:06:32 +0000

    funcao testar combinacoes

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 typedef struct {
     char nome[10];
@@ -8,11 +9,41 @@ typedef struct {
     float retorno;
 } acao;
 
+int investimento (acao *array, int n, float capital, float *melhorCusto, float *maiorRetorno){
+    int  totalcomb = pow(2, n);
+    int melhorcomb;
+    *melhorCusto = 0;
+    *maiorRetorno = -1;
+
+    for(int comb = 1; comb < totalcomb; comb++){
+        float custototal = 0;
+        float retornototal = 0;
+
+        for(int j = 0; j < n; j++){
+            if(comb &(1 << j)){
+                custototal += array[j].custo;
+                retornototal += array[j].retorno;
+
+            }
+        }
+        if (custototal<=capital && retornototal > *maiorRetorno)
+        {
+            *maiorRetorno = retornototal;
+            *melhorCusto =  custototal;
+            melhorcomb = comb;
+        }
+    }
+    return melhorcomb;
+}
+
 int main() {
 
     int n;
+    float capital;
     printf("Quantas acoes: ");
     scanf("%d", &n);
+    printf("Qual o capital inicial");
+    scanf("%d", &capital);
 
     acao *array = malloc(n*sizeof(acao));
 
