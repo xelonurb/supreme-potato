f111a9802b2739efc0ffb8568b319e695018b866
xelonurb <163100560+xelonurb@users.noreply.github.com>
2025-06-20 00:09:40 +0000

    so precisa multiplicar o custo das acoes por 100 e mudar de float para int, nao esquecer de mudar na saida tambem

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 typedef struct {
-    char nome[10];
+    char nome[30];
     float custo;
     float retorno;
 } acao;
@@ -35,6 +35,42 @@ int investimento (acao *array, int n, float capital, float *melhorCusto, float *
     return melhorcomb;
 }
 
+float mochilaknapsack(acao *array, int n, int capital, int *selecionadas){
+    float **dp = malloc((n + 1) * sizeof(float *));
+    for(int i = 0; i <= n; i++){
+        dp[i] = calloc((capital + 1), sizeof(float));
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j <= capital; j++){
+            if(array[i-1].custo > j){
+                dp[i][j] = dp[i - 1][j];
+            }
+            else {
+                float naoPega = dp[i - 1][j];
+                float pega = dp[i - 1][j - (int)array[i - 1].custo] + array[i-1].retorno;
+                dp[i][j] = (pega > naoPega) ? pega : naoPega;
+            }
+        }
+    }
+    int restante = capital;
+    for(int i = n; i > 0; i--){
+        if(dp[i][restante] != dp[i - 1][restante]){
+            selecionadas[i - 1] = 1;
+            restante -= array[i - 1].custo;
+        }else{
+            selecionadas[i - 1] = 0;
+        }
+    }
+    float resultado = dp[n][capital];
+    for(int i = 0; i <= n; i++){
+        free(dp[i]);
+
+    }
+    free(dp);
+    return resultado;
+}
+
+
 void exibirResultado(acao *array, int n, float capital, int melhorcomb, float melhorCusto, float maiorRetorno ){
 
     printf("\n----------------------------------------\n");
@@ -53,8 +89,29 @@ void exibirResultado(acao *array, int n, float capital, int melhorcomb, float me
     printf("- Custo Total: R$ %.2f\n", melhorCusto);
     printf("- Retorno Máximo Esperado: %.2f%%\n", maiorRetorno);
     printf("----------------------------------------\n");
+}
 
 
+
+void exibirResultadoPD(acao *array, int n, float capital, int *selecionadas, float retornoMaximo){
+    printf("\n----------------------------------------\n");
+    printf("Carteira de Investimentos Otimizada\n");
+    printf("----------------------------------------\n");
+    printf("Capital Disponível: R$ %.2f\n\n", capital);
+    printf("Ações a Comprar:\n");
+
+    float custoTotal = 0;
+    for(int i = 0; i < n; i++){
+        if(selecionadas[i]){
+            printf("- %s (Custo: R$ %.2f, Retorno: %.2f%%)\n", array[i].nome, array[i].custo, array[i].retorno);
+            custoTotal += array[i].custo;
+        }
+    }
+
+    printf("\nResumo da Carteira:\n");
+    printf("- Custo Total: R$ %.2f\n", custoTotal);
+    printf("- Retorno Máximo Esperado: %.2f%%\n", retornoMaximo);
+    printf("----------------------------------------\n");
 }
 int main() {
 
@@ -81,11 +138,16 @@ int main() {
     for(int i = 0; i< n; i++){
         printf("%-20s | %10.2f | Retorno: %9.2f%%\n", array[i].nome, array[i].custo, array[i].retorno);
     }
-
+    /*
     float melhorCusto, maiorRetorno;
     int melhorcomb = investimento(array, n, capital, &melhorCusto, &maiorRetorno);
-    exibirResultado(array, n, capital, melhorcomb, melhorCusto, maiorRetorno);
-
+     exibirResultado(array, n, capital, melhorcomb, melhorCusto, maiorRetorno); 
+    */
+   //com a mochila de knapsack
+   int *selecionadas = calloc(n, sizeof(int));
+   float retornoMaximo = mochilaknapsack(array, n, (int)capital, selecionadas);
+   exibirResultadoPD(array, n, capital, selecionadas, retornoMaximo);
     free(array);
+    free(selecionadas);
     return 0;
 }
